51490123072e3fb2f530c01af0a03de201a00ddf
Carlos Guimaraes <carlos.em.guimaraes@gmail.com>
2021-03-26 09:56:17 +0100

    Fixed niryo one web interface docker integration

--- digital-twin-service/web-interface/run_container.sh
+++ digital-twin-service/web-interface/run_container.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 
 # Localhost Networking settings
-ROS_MASTER_URI="http://10.0.2.193:11311"
-ROS_IP="10.0.2.198"
-
-sudo docker build . -t niryo-one-web
+ROS_MASTER_URI="http://127.0.0.1:11311"
+ROS_IP="127.0.0.1"
 
 # Networking settings simulated robot
-#sudo docker run \
-#       --hostname dtwin-web \
-#       -it \
-#       --rm \
-#       --net=robot_eth_macvlan \
-#        --ip=10.0.2.198 \
-#       -e DISPLAY=$DISPLAY \
-#        -e ROS_MASTER_URI=$ROS_MASTER_URI \
-#        -e ROS_IP=$ROS_IP \
-#       -e XAUTHORITY=$XAUTH \
-#       -v $XSOCK:$XSOCK  \
-#       -v $XAUTH:$XAUTH \
-#       --add-host niryo-one-control:10.0.2.194 \
-#       --add-host niryo-one-motion:10.0.2.195 \
-#       --add-host niryo-one-interface:10.0.2.196 \
-#       --add-host niryo-one-sim:10.0.2.193 \
-#       --add-host dtwin-web:127.0.0.1 \
-#       --add-host dtwin:10.0.2.197 \
-#       dtwin-web:latest \
+sudo docker run \
+       --hostname niryo-one-web \
+       --rm \
+       --net=host \
+       -e ROS_MASTER_URI=$ROS_MASTER_URI \
+       -e ROS_IP=$ROS_IP \
+       --add-host niryo-one-control:127.0.0.1 \
+       --add-host niryo-one-motion:127.0.0.1 \
+       --add-host niryo-one-interface:127.0.0.1 \
+       --add-host niryo-one-sim:127.0.0.1 \
+       --add-host niryo-one-web:127.0.0.1 \
+       --add-host dtwin:127.0.0.1 \
+       niryo-one-web:latest
